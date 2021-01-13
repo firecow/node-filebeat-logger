a7e5885353d70cf7c02174f8dfacb2a0b230d9e7
Mads Jon Nielsen <madsjon@gmail.com>
2021-01-13 07:57:34 +0100

    Added three check log level tests

--- src/filebeat-logger-factory.test.ts
+++ src/filebeat-logger-factory.test.ts
@@ -17,28 +17,60 @@ afterAll(() => {
     MockDate.reset();
 })
 
-const logger = create({logLevel: 'debug'});
-
 test('Check logger debug default streams', () => {
+    const logger = create({logLevel: 'debug'});
     logger.debug('Text Message');
     expect(spyStdout).toHaveBeenLastCalledWith(`{"@timestamp":"2019-05-14T11:01:58.135Z","message":"Text Message","log.level":"debug"}\n`);
     expect(spyStderr).toBeCalledTimes(0)
 });
 
 test('Check logger info default streams', () => {
+    const logger = create({logLevel: 'debug'});
     logger.info('Text Message');
     expect(spyStdout).toHaveBeenLastCalledWith(`{"@timestamp":"2019-05-14T11:01:58.135Z","message":"Text Message","log.level":"info"}\n`);
     expect(spyStderr).toBeCalledTimes(0)
 });
 
 test('Check logger warning default streams', () => {
+    const logger = create({logLevel: 'debug'});
     logger.warn('Text Message');
     expect(spyStderr).toHaveBeenLastCalledWith(`{"@timestamp":"2019-05-14T11:01:58.135Z","message":"Text Message","log.level":"warn"}\n`);
     expect(spyStdout).toBeCalledTimes(0)
 })
 
 test('Check logger error default streams', () => {
+    const logger = create({logLevel: 'debug'});
     logger.error('Text Message');
     expect(spyStderr).toHaveBeenLastCalledWith(`{"@timestamp":"2019-05-14T11:01:58.135Z","message":"Text Message","log.level":"error"}\n`);
+    expect(spyStdout).toBeCalledTimes(0)
+})
+
+test('Check log level info', () => {
+    const logger = create({logLevel: 'info'});
+    logger.debug('Text Message');
+    logger.info('Text Message');
+    logger.warn('Text Message');
+    logger.error('Text Message');
+    expect(spyStderr).toBeCalledTimes(2)
+    expect(spyStdout).toBeCalledTimes(1)
+})
+
+test('Check log level warn', () => {
+    const logger = create({logLevel: 'warn'});
+    logger.debug('Text Message');
+    logger.info('Text Message');
+    logger.warn('Text Message');
+    logger.error('Text Message');
+    expect(spyStderr).toBeCalledTimes(2)
+    expect(spyStdout).toBeCalledTimes(0)
+})
+
+test('Check log level error', () => {
+    const logger = create({logLevel: 'error'});
+    logger.debug('Text Message');
+    logger.info('Text Message');
+    logger.warn('Text Message');
+    logger.error('Text Message');
+    expect(spyStderr).toBeCalledTimes(1)
     expect(spyStdout).toBeCalledTimes(0)
 })
